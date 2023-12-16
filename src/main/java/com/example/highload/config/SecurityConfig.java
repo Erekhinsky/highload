a6b3ef00d95c98fbc4c5872ee862cf20596151d3
Sofya Inglikova <sofiainglikova@gmail.com>
2023-12-16 19:25:02 +0300

    fix: fix

--- src/main/java/com/example/highload/config/SecurityConfig.java
+++ src/main/java/com/example/highload/config/SecurityConfig.java
@@ -35,7 +35,7 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.csrf(AbstractHttpConfigurer::disable)
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                .authorizeHttpRequests(request -> request.requestMatchers("/api/app/user/login", "/api/app/user/register", "/error")
+                .authorizeHttpRequests(request -> request.requestMatchers("/api/user/login", "/api/user/register", "/error")
                         .permitAll()
                         .anyRequest()
                         .authenticated())
